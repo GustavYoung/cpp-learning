70a64d2500b5e5bc4bfe45d6fe375bd111cc0db0
zenAndroid <aminezenkouar@gmail.com>
2016-09-13 14:30:24 +0100

    Exo : fonction qui dessine un rectangle

--- second/main.cpp
+++ second/main.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+void dessineRectangle(int largeur,int hauteur)
+{
+    for(int i(1);i<=hauteur;i++)
+    {
+        for(int j(1);j<=largeur;j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
+    int large = 0;
+    int haut = 0;
     cout << "Hello world!" << endl;
+    cout << "Entrez l'hauteur : " << endl;
+    cin >> haut;
+    cout << "Entrez la largeur : " << endl;
+    cin >> large;
+    dessineRectangle(large,haut);
     return 0;
 }
