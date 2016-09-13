e61b33a6fde058731bd3f5afbde6720b5b0344c6
zenAndroid <aminezenkouar@gmail.com>
2016-09-13 14:36:00 +0100

    Petite modif et/ou amélioraton

--- second/main.cpp
+++ second/main.cpp
@@ -19,10 +19,14 @@ int main()
     int large = 0;
     int haut = 0;
     cout << "Hello world!" << endl;
-    cout << "Entrez l'hauteur : " << endl;
-    cin >> haut;
-    cout << "Entrez la largeur : " << endl;
-    cin >> large;
+    do{
+        cout << "Entre une hauteur valide (strictement positive) ." << endl;
+        cin >> haut;
+    }while(haut<=0);
+    do{
+        cout << "Entrez une valeur de largeur valide (strictement postive) ." << endl;
+        cin >> large;
+    }while(large<=0);
     dessineRectangle(large,haut);
     return 0;
 }
