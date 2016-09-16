da6742176fd39b2fb08d0dbe2d11563079f9c107
zenAndroid <aminezenkouar@gmail.com>
2016-09-16 17:09:23 +0100

    Messing with pointers

---
+++ pointers/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    std::string doot;
+    cout << "Hello world!" << endl;
+    cout << &doot << endl;
+    return 0;
+}
