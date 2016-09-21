f583022af9dfd5288bcb9a979ec47395c173ec0b
zenAndroid <aminezenkouar@gmail.com>
2016-09-21 22:25:39 +0100

    Done for now, will overload for ints and doubles and will generaly improve the class as time goes on :)

---
+++ experimenting/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string kek("");
+    cout << kek << endl;
+    kek = "1";
+    cout << kek << endl;
+    cout << kek << endl;
+    cout << "Hello world!" << endl;
+    return 0;
+}
